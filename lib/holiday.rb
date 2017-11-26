d567a36571563d7b7742c7c63562eb822ad6afd6
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-26 03:41:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
         words = day.to_s.split("_")
         words.each do |word|
           word.capitalize!
-          joined = words
+          joined = words.join(" ")
           end
         else
           joined = day.to_s
