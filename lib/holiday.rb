c4a61af92351025d3f7aeaa61948b13b5099c588
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-26 02:39:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,8 +87,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season|
     season.each do |holiday|
-      holiday.each do |day|
       binding.pry
+      holiday.each do |day|
+      #binding.pry
       end
     end
   end
